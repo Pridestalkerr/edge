c7fcbef95c37532d24db76cd778d80bb52aa1125
pridestalkerr <danartenie158@gmail.com>
2024-05-21 16:12:29 +0300

    comment for procedures

--- packages/api/src/procedures.ts
+++ packages/api/src/procedures.ts
@@ -1,5 +1,7 @@
 import { baseProtectedProcedureMiddleware } from "./middleware";
 import { trpc } from "./trpc";
 
+// NOTE: Consider rate limiting the public procedure.
+// TODO: example
 export const publicProcedure = trpc.procedure;
 export const protectedProcedure = trpc.procedure.use(baseProtectedProcedureMiddleware);
