b6a517ee9124cd1be85ab68cac1bfe3e2d43d545
pridestalkerr <danartenie158@gmail.com>
2024-05-21 16:09:35 +0300

    trpc server setup

--- packages/api/src/trpc.ts
+++ packages/api/src/trpc.ts
@@ -3,33 +3,70 @@ import superjson from "superjson";
 import { ZodError } from "zod";
 import type { CreateExpressContextOptions } from "@trpc/server/adapters/express";
 import { env } from "@edge/env";
-import type { response, request } from "express";
+import type { Request, Response } from "express";
 import type { OpenApiMeta } from "trpc-openapi";
+import { lucia } from "@edge/auth";
 
-interface Session {
-  token: string;
+interface Auth {
+  session: Awaited<ReturnType<typeof lucia.validateSession>>["session"];
+  user: Awaited<ReturnType<typeof lucia.validateSession>>["user"];
 }
 
 export interface Context {
-  session: Session;
-  req: typeof request;
-  res: typeof response;
+  auth: Auth;
+  req: Request;
+  res: Response;
+  cookie: Response["cookie"];
+  cookies: Record<string, string>;
 }
 
 export const createTRPCContext = async ({
   req,
   res,
-  // eslint-disable-next-line @typescript-eslint/require-await
 }: CreateExpressContextOptions): Promise<Context> => {
+  // https://lucia-auth.com/guides/validate-session-cookies/
+  // NOTE: if you don't need validation on every request, move this to the `baseProtectedProcedureMiddleware` middleware
   const cookies = req.cookies as Record<string, string>;
   const token = cookies.token;
 
+  const def = {
+    req,
+    res,
+    cookie: res.cookie.bind(res),
+    cookies,
+  };
+
+  if (!token) {
+    return {
+      ...def,
+      auth: { session: null, user: null },
+    };
+  }
+
+  // this refreshes automatically when needed
+  const { user, session } = await lucia.validateSession(token);
+  if (!session) {
+    return {
+      ...def,
+      auth: { session: null, user: null },
+    };
+  }
+
+  if (session.fresh) {
+    // set the new cookie on the client
+    const sessionCookie = lucia.createSessionCookie(session.id);
+    res.cookie(sessionCookie.name, sessionCookie.serialize(), sessionCookie.attributes);
+  }
+
   return {
-    session: {
-      token: token ?? "",
+    auth: {
+      session,
+      user,
     },
     req,
     res,
+    cookie: res.cookie.bind(res),
+    cookies,
   };
 };
 
