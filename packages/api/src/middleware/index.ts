d36e55832de7d99540082c03c8ed68bb7cad3560
pridestalkerr <danartenie158@gmail.com>
2024-05-21 16:12:11 +0300

    small change to base protected procedure

--- packages/api/src/middleware/index.ts
+++ packages/api/src/middleware/index.ts
@@ -2,19 +2,15 @@ import { TRPCError } from "@trpc/server";
 import { trpc } from "../trpc";
 
 export const baseProtectedProcedureMiddleware = trpc.middleware(async ({ ctx, next }) => {
-  if (!ctx.session) {
+  if (!ctx.auth.session || !ctx.auth.user) {
     throw new TRPCError({ message: "Unauthorized. You must be logged in.", code: "UNAUTHORIZED" });
   }
-
-  // do your user validation here
-  // e.g
-  // const user = await db.findUser({ token: ctx.session.token });
 
   return next({
     ctx: {
       ...ctx,
-      // infers the `session` as non-nullable
-      session: ctx.session,
+      // infers the session as non-nullable
+      auth: ctx.auth,
     },
   });
 });
