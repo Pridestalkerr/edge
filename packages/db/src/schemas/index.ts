127cabd22aacaaa004b1130ba20c87d362f141b4
pridestalkerr <danartenie158@gmail.com>
2024-05-21 16:25:49 +0300

    drizzle setup

---
+++ packages/db/src/schemas/index.ts
@@ -0,0 +1,10 @@
+import * as relations from "./relations";
+import * as common from "./common";
+
+export * from "./relations";
+export * from "./common";
+
+export default {
+  ...relations,
+  ...common,
+};
