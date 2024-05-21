4f7614204f001b0f6c557ba8baa7d0aa18b18806
pridestalkerr <danartenie158@gmail.com>
2024-05-21 07:56:09 +0300

    unplugin icons setup for nextjs

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,8 +1,22 @@
 //TODO: wait for next.config.ts support...
-// import "@edge/env"
-import "@edge/env/src/index.mjs"
+// import "@edge/env";
+import "@edge/env/src/index.mjs";
+import Icons from "unplugin-icons/webpack";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  // https://github.com/unplugin/unplugin-icons?tab=readme-ov-file#build-tools
+  webpack(config) {
+    config.plugins.push(
+      Icons({
+        compiler: "jsx",
+        jsx: "react",
+      }),
+    );
+
+    return config;
+  },
+};
 
 export default nextConfig;
