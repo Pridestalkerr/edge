136d26e48e2134eeff1b2e38bb8fd43e8894b772
pridestalkerr <danartenie158@gmail.com>
2024-12-15 17:28:52 +0200

    Add OpenAPI document generation function to API

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,3 +1,4 @@
+import { generateOpenApiDocument } from "@edge-placeholder/trpc-openapi";
 import type { AppRouter, RouterInputs, RouterOutputs } from "./root";
 import { appRouter } from "./root";
 import { createTRPCContext } from "./trpc";
@@ -9,3 +10,15 @@ export type { AppRouter };
 export { appRouter };
 
 export { createTRPCContext };
+
+/**
+ * @description Generate OpenAPI document. You can add more params to this function depending on your needs.
+ */
+export const openApiDocument = (baseUrl: string) => {
+  return generateOpenApiDocument(appRouter, {
+    title: "Edge API",
+    version: "1.0.0",
+    baseUrl,
+    docsUrl: "https://github.com/jlalmes/trpc-openapi",
+  });
+};
