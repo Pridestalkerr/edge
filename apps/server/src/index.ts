e2a5eda0cd9c972c5c94dbe4e21c2b465fdd7c1f
pridestalkerr <danartenie158@gmail.com>
2024-12-15 17:29:21 +0200

    Add OpenAPI middleware and Swagger UI integration to the server

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,10 +1,12 @@
 import { createServer } from "http";
-import { appRouter, createTRPCContext } from "@edge-placeholder/api";
+import { appRouter, createTRPCContext, openApiDocument } from "@edge-placeholder/api";
 import { server as env } from "@edge-placeholder/env";
+import { createOpenApiExpressMiddleware } from "@edge-placeholder/trpc-openapi";
 import { createExpressMiddleware } from "@trpc/server/adapters/express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import express from "express";
+import swagger from "swagger-ui-express";
 
 // eslint-disable-next-line @typescript-eslint/require-await
 const main = async () => {
@@ -20,8 +22,8 @@ const main = async () => {
   app.use(cookieParser());
 
   const trpcEP = env.API_ENDPOINT + "/trpc";
-  // const restEP = env.API_ENDPOINT + "rest";
-  // const swaggerEP = env.API_ENDPOINT + "swagger";
+  const restEP = env.API_ENDPOINT + "/rest";
+  const swaggerEP = env.API_ENDPOINT + "/swagger";
 
   app.use(
     trpcEP,
@@ -31,13 +33,25 @@ const main = async () => {
     }),
   );
 
+  // TODO: you MUST implement CSRF protection for all exposed GET endpoints!
+  app.use(
+    restEP,
+    createOpenApiExpressMiddleware({
+      router: appRouter,
+      createContext: createTRPCContext,
+    }),
+  );
+
+  app.use(swaggerEP, swagger.serve);
+  app.get(swaggerEP, swagger.setup(openApiDocument(restEP)));
+
   // const server = createServer(app);
 
   app.listen(env.API_PORT, () => {
     console.log(`Express: \n\t http://localhost:${env.API_PORT}`);
     console.log(`tRPC: \n\t http://localhost:${env.API_PORT}${trpcEP}`);
-    // console.log(`REST interface: \n\t http://localhost:${env.API_PORT}${restEP}`);
-    // console.log(`Swagger: \n\t http://localhost:${env.API_PORT}${swaggerEP}`);
+    console.log(`REST interface: \n\t http://localhost:${env.API_PORT}${restEP}`);
+    console.log(`Swagger: \n\t http://localhost:${env.API_PORT}${swaggerEP}`);
   });
 };
 
