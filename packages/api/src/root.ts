682761b662c9c6938a6236ad8019925d28ec62da
pridestalkerr <danartenie158@gmail.com>
2024-12-15 17:29:14 +0200

    Enhance tRPC compatibility layer for v11 and improve developer experience

--- packages/api/src/root.ts
+++ packages/api/src/root.ts
@@ -1,4 +1,6 @@
 import type { inferRouterInputs, inferRouterOutputs } from "@trpc/server";
+import type { AnyProcedure } from "@trpc/server/unstable-core-do-not-import";
+import { z } from "zod";
 import { testRouter } from "./router/test";
 import { router } from "./trpc";
 
@@ -9,5 +11,37 @@ const appRouter = router({
 export type AppRouter = typeof appRouter;
 export type RouterInputs = inferRouterInputs<AppRouter>;
 export type RouterOutputs = inferRouterOutputs<AppRouter>;
+
+/**
+ * @description This is entirely optional so you can remove it, or consider moving to another file. Its a temporary compatibility layer for tRPC v11 and a DX improvement
+ * @see https://github.com/trpc/trpc-openapi/issues/463
+ * */
+const procedures = appRouter._def.procedures;
+Object.keys(procedures).forEach((key) => {
+  const def = (procedures[key as keyof typeof procedures] as unknown as AnyProcedure)?._def;
+  // @ts-expect-error: internal API
+  if (def?.meta?.openapi) {
+    switch (def.type) {
+      case "query":
+        // @ts-expect-error: unstable support for tRPC v11
+        def.query = true;
+        break;
+      case "mutation":
+        // @ts-expect-error: unstable support for tRPC v11
+        def.mutation = true;
+        break;
+      case "subscription":
+        // @ts-expect-error: unstable support for tRPC v11
+        def.subscription = true;
+        break;
+    }
+
+    // @ts-expect-error: hack to make all openapi procedures have an ANY output schema unless explictitly defined (DX improvement)
+    if (!def.output) {
+      // @ts-expect-error: see above
+      def.output = z.any();
+    }
+  }
+});
 
 export { appRouter };
